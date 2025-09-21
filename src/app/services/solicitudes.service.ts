f3dba5912b3cbd7ddcf45e354374a3eb1ff5923d
AlejandraPacheco <alejandra.pacheco1812@gmail.com>
2025-09-21 01:43:44 -0400

    Solicitante Dashboard completed

---
+++ src/app/services/solicitudes.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { DashboardSolicitanteDto } from '../models/DashboardSolicitanteDto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SolicitudesService {
+  private apiUrl = 'http://localhost:8081/api/solicitudes';
+
+  constructor(private http: HttpClient) {}
+
+  // Trae las solicitudes de un usuario
+  getSolicitudesByUsuario(idUsuario: number): Observable<DashboardSolicitanteDto[]> {
+    return this.http.get<DashboardSolicitanteDto[]>(`${this.apiUrl}/usuario/${idUsuario}`);
+  }
+}
